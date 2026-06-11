785907fe2b2e62f600c8aacd6c37b18289d0a7df
Antwan Grant <twanjobs68@gmail.com>
2026-06-10 20:52:21 -0400

    second commit

---
+++ lab3.js
@@ -0,0 +1,13 @@
+//Task 1a
+// let randomNumber = Math.floor(Math.random() * 100) + 1;
+// console.log("Random numbers are: " +  randomNumber);
+
+//Task 1b
+// let radius = 5;
+// let area = Math.PI * Math.pow(radius, 2);
+
+console.log(area);
+let radius = 5;
+let area = Math.PI * (radius ** 2);
+
+console.log(area.toFixed(2));
